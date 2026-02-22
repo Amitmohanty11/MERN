00cd0d615a2905cde7b4d8e66bb5ed539a43f0f0
Amit Mohanty <daamitmohanty1111@gmail.com>
2026-02-22 11:42:00 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,20 +1,15 @@
-import Navbar from "./Components/Navbar"
-import Footer from "./Components/Footer"
-import Card from "./Components/Card"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
-    <main>
-      <div className="cards">
-        <Card title = "Card 1" description = "This is card 1 using props"/>
-        <Card title = "Card 2" description = "This is card 2 using props"/>
-        <Card title = "Card 3" description = "This is card 3 using props"/>
-        <Card title = "Card 4" description = "This is card 4 using props"/>
-      </div>
-    </main>
-    <Footer/>
+      <div>the count is {count}</div>
+      <button onClick={()=>{setCount(count+1)}}>click here</button>
     </>
   )
 }
